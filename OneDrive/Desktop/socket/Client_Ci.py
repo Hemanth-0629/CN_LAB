ef750ab9f2af96a45689783b5a515d76884bdf07
hemanth <gurramhemanth0629@gmail.com>
2023-11-02 13:31:31 +0530

    update

--- OneDrive/Desktop/socket/Client_Ci.py
+++ OneDrive/Desktop/socket/Client_Ci.py
@@ -14,4 +14,4 @@ if client_name in ['S0', 'S1', 'S2', 'S3', 'S4']:
 
     client_socket.close()
 else:
-    print("Invalid server name. Please enter S0, S1, S2, S3, S4.")
+    print("Invalid server name. Please enter the servers S0, S1, S2, S3, S4.")
