24507e8fd9a8418334cebf9dfe868d416e557a25
hemanth <gurramhemanth0629@gmail.com>
2023-11-03 14:01:42 +0530

    update

--- OneDrive/Desktop/socket/client_s.py
+++ OneDrive/Desktop/socket/client_s.py
@@ -5,6 +5,7 @@ SERVER_ADDRESS = ('localhost', 12345)
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect(SERVER_ADDRESS)
 
+
 client_name = input("Enter client name: ")
 socket.send(client_name.encode())
 
