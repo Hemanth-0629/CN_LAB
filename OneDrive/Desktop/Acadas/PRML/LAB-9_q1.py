138a708fbcc2e8c5bf274e17159681ba2dada7a4
hemanth <gurramhemanth0629@gmail.com>
2025-09-28 12:09:43 +0530

    update

--- OneDrive/Desktop/Acadas/PRML/LAB-9_q1.py
+++ OneDrive/Desktop/Acadas/PRML/LAB-9_q1.py
@@ -57,6 +57,7 @@ def bayes_classifier(X_train, y_train, X_test):
 predictions = bayes_classifier(X_train_lda, y_train, X_test_lda)
 
 
+
 # Calculate accuracy
 accuracy = np.mean(predictions == y_test)
 print("Accuracy:", accuracy)
